8dc9beaa075921dfad3992445eb0658f89515a65
ibalram <balramsinghrathore3@gmail.com>
2020-11-12 02:25:01 +0530

    Added: Ability to add new contacts to address book

--- AddressBookSystem.js
+++ AddressBookSystem.js
@@ -1,10 +1,14 @@
 const Contact = require('./Contact.js');
 console.log("Welcome Address Book System");
 
+
+let addressBook = new Array();
+
 try{
-    let contact = new Contact("Balram", "Singh", "no location","Jaipur", "Rajasthan", "301234", "91 9812345678","bal@gmail.com");
-    console.log(contact);
+    addressBook.push(new Contact("Balram", "Singh", "no location","Jaipur", "Rajasthan", "301234", "91 9812345678","bal@gmail.com"));
+    addressBook.push(new Contact("Akshit", "Singh", "no location","Mumbai", "Maharashtra", "401234", "91 9912345678","akshit@gmail.com"));
 }
 catch(e){
     console.error(e);
 }
+console.log(addressBook);
